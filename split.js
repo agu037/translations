0bddc1243195e750b75391859ab9be0058356dcf
Alvin Haukedal Gusfre <alvinh.gusfre@Alvins-MacBook-Pro-2.local>
2022-10-14 13:53:13 +0200

    norwegian

--- split.js
+++ split.js
@@ -1,9 +1,17 @@
 const fs = require('fs')
-const en = require('./en.json')
+const lang = require('./no.json')
 
-for(const [scope, keys] of Object.entries(en)) {
+for(const [scope, keys] of Object.entries(lang)) {
   try {
-    fs.writeFileSync(`locales/${scope}.json`, JSON.stringify({ [scope]:keys }, null, 2))
+    var defaults = require(`locales/${scope}.json`)
+  } catch(err) {
+    continue
+  }
+  for(const [key, value] of Object.entries(keys[scope])) {
+    if(value == keys[key]) delete keys[key]
+  }
+  try {
+    fs.writeFileSync(`locales/nb/${scope}.json`, JSON.stringify({ [scope]:keys }, null, 2))
   } catch(err) {
     console.warn(err)
   }
